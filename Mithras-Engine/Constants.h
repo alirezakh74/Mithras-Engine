b7228553f8a6845c75ea3265064a90b71961ad5b
alireza khodabande <alirezakhodabande74@gmail.com>
2022-01-03 23:17:39 +0330

    limit frame rate consts

--- Mithras-Engine/Constants.h
+++ Mithras-Engine/Constants.h
@@ -4,3 +4,6 @@ const unsigned int WINDOW_WIDTH = 800;
 const unsigned int WINDOW_HEIGHT = 600;
 const char* WINDOW_TITLE = "Mithras Engine";
 
+const int FPS = 60; // 60 frames per second
+const float TIME_PER_FRAME = 1000.0 / FPS;
+
