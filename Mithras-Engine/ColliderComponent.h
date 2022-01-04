e4fa5db5021e9b34ec087a8dcef85a489e29f02b
alireza khodabande <alirezakhodabande74@gmail.com>
2022-01-05 02:51:26 +0330

    added collider box

---
+++ Mithras-Engine/ColliderComponent.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "Component.h"
+#include "SDL2/SDL.h"
+
+class ColliderComponent : public Component
+{
+public:
+	SDL_Rect colliderRect;
+
+	virtual void init() override
+	{
+
+	}
+
+	virtual void update(float deltaTime) override
+	{
+
+	}
+
+	virtual void render() override
+	{
+
+	}
+};
