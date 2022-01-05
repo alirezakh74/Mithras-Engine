35c0243b0506bf7dc7a44b05a0cb589a0cdb8ca8
alireza khodabande <alirezakhodabande74@gmail.com>
2022-01-05 21:34:00 +0330

    f

--- Mithras-Engine/Constants.h
+++ Mithras-Engine/Constants.h
@@ -5,5 +5,5 @@ const unsigned int WINDOW_HEIGHT = 600;
 const char* WINDOW_TITLE = "Mithras Engine";
 
 const int FPS = 60; // 60 frames per second
-const float TIME_PER_FRAME = 1000.0 / FPS;
+const float TIME_PER_FRAME = 1000.0f / FPS;
 
