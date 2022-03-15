d765ba71b3d7c3c3d0db96eb73b2ff9e8bed96e2
alireza khodabande <alirezakhodabande74@gmail.com>
2022-03-15 20:41:27 +0330

    add map of type component and has and get component functions

--- Mithras-Engine/Entity.h
+++ Mithras-Engine/Entity.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <vector>
+#include <map>
 #include <string>
 #include <typeinfo>
 
@@ -22,25 +23,31 @@ public:
 		T* newComponent(new T(std::forward<TArgs>(args)...));
 		newComponent->owner = this;
 		m_components.emplace_back(newComponent);
+		mapComponentType[&typeid(*newComponent)] = newComponent;
 		newComponent->init();
 		return *newComponent;
 	}
 
 	template<typename T>
-	T& getComponent()
+	T* getComponent()
 	{
-		for (auto& component : m_components)
+		return static_cast<T*>(mapComponentType[&typeid(T)]);
+	}
+
+	template<typename T>
+	bool hasComponent()
+	{
+		for (auto map : mapComponentType)
 		{
-			//std::cout << "component type is: " << typeid(dynamic_cast<T*>(component)).name() << "\n";
-			//std::cout << "component type is: " << typeid(T*).name() << "\n";
-			if (typeid(dynamic_cast<T*>(component)) == typeid(T*))
+			//std::string mapFirst = map.first->name();
+			//std::string typeID = typeid(T).name();
+			if (map.first->name() == typeid(T).name())
 			{
-				return dynamic_cast<T&>(*component);
-				//return (T&)(*component);
+				return true;
 			}
 		}
 
-		//return NullComponent;
+		return false;
 	}
 
 	void update(float deltaTime);
@@ -49,6 +56,7 @@ public:
 	bool isActive() const { return m_bIsActive; }
 
 	std::vector<Component*> getComponents() const { return m_components; }
+	std::map<const std::type_info*, Component*> mapComponentType;
 	std::string  getName() const { return m_name; }
 
 private:
